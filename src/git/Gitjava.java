46c959714cb5e4c7d632596b7370c62f5771c785
lmourine018 <lmourine8@gmail.com>
2022-09-22 16:29:12 +0300

    learning java

---
+++ src/git/Gitjava.java
@@ -0,0 +1,10 @@
+package git;
+
+public class Gitjava {
+    public static void main(String[] args) {
+        int count = 10;
+        int var = 9;
+        double sum = count + var;
+        System.out.println( count + var);
+    }
+}
